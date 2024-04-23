3881e9b40dbd2af0d84c48e6b7a49b37863eaa6a
Mark Joseph Jagyo <mark.jagyo@gmail.com>
2024-04-24 00:05:18 +0800

    Updated README.md

--- README.md
+++ README.md
@@ -1,28 +1,42 @@
-# demo-plugin
+# InfluxDB Plugin
 
 ## Introduction
 
-TODO Describe what your plugin does here
+The InfluxDB Plugin is designed to seamlessly integrate Jenkins with InfluxDB. It facilitates the posting of detailed Jenkins job data into an InfluxDB database, enabling enhanced monitoring and analytics of CI/CD pipelines. This plugin collects comprehensive metrics from Jenkins jobs, including job name, build status, build duration, build number, and build time, along with the hierarchical folder structure of each job. These metrics are crucial for real-time and historical analysis of build performance, helping teams to optimize their development processes.
 
 ## Getting started
+To get started with the InfluxDB Plugin, follow these steps to install and configure the plugin in your Jenkins environment:
 
-TODO Tell users how to configure your plugin here, include screenshots, pipeline examples and 
-configuration-as-code examples.
+### Installation
+- Navigate to your Jenkins dashboard.
+- Go to Manage Jenkins > Manage Plugins.
+- Click on the Available tab and search for “InfluxDB Plugin”.
+- Select the plugin and click Install.
+
+### Configuration
+After installation, you need to configure the plugin to connect to your InfluxDB instance:
+
+- Go to Manage Jenkins > Configure System.
+- Scroll down to InfluxDB Targets and click Add InfluxDB.
+- Configure the InfluxDB target with the following parameters:
+    - Description: A brief description of the database connection.
+    - URL: The URL of your InfluxDB instance (e.g., http://localhost:8086).
+    - Database: The name of your InfluxDB database where metrics will be stored.
+    - User: Username for the database (if authentication is enabled).
+    - Password: Password for the database (if authentication is enabled).
 
 ## Issues
 
-TODO Decide where you're going to host your issues, the default is Jenkins JIRA, but you can also enable GitHub issues,
-If you use GitHub issues there's no need for this section; else add the following line:
+For tracking issues and enhancements, please use the Jenkins issue tracker. Contributions, feature requests, and bug reports are all welcomed here:
 
-Report issues and enhancements in the [Jenkins issue tracker](https://issues.jenkins.io/).
+[Report issues and enhancements in the Jenkins issue tracker.](https://issues.jenkins.io/)
 
 ## Contributing
 
-TODO review the default [CONTRIBUTING](https://github.com/jenkinsci/.github/blob/master/CONTRIBUTING.md) file and make sure it is appropriate for your plugin, if not then add your own one adapted from the base file
-
-Refer to our [contribution guidelines](https://github.com/jenkinsci/.github/blob/master/CONTRIBUTING.md)
+Contributions to the InfluxDB Plugin are welcomed! To contribute, please follow the guidelines provided in our [contribution guidelines](https://github.com/jenkinsci/.github/blob/master/CONTRIBUTING.md)
 
 ## LICENSE
 
-Licensed under MIT, see [LICENSE](LICENSE.md)
+Licensed under MIT, see [LICENSE](LICENSE.md) for more information.
+
 
--- src/main/java/io/jenkins/plugins/sample/InfluxDBPublisher.java
+++ src/main/java/io/jenkins/plugins/sample/InfluxDBPublisher.java
@@ -10,6 +10,7 @@ import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
+import hudson.model.Job;
 import hudson.model.Result;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Publisher;
@@ -18,10 +19,12 @@ import hudson.util.FormValidation;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.ServletException;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
-import java.util.Random;
 
 public class InfluxDBPublisher extends Recorder {
 
@@ -32,6 +35,7 @@ public class InfluxDBPublisher extends Recorder {
     private String adminToken;
     private String organizationId;
     private String bucketName;
+    private String numberStr = "";
 
     @DataBoundConstructor
     public InfluxDBPublisher(String name) {
@@ -80,7 +84,7 @@ public class InfluxDBPublisher extends Recorder {
 
     public static double getRandomBuildDuration() {
         // Generate a double between 0.0 (inclusive) and 7.0 (exclusive)
-        double duration = 7 * random.nextDouble(); 
+        double duration = 7 * random.nextDouble();
 
         // Shift the range to 8.0 to 15.0
         duration += 8;
@@ -89,12 +93,13 @@ public class InfluxDBPublisher extends Recorder {
         return Math.round(duration * 100.0) / 100.0;
     }
 
-
     @Override
     public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
             throws InterruptedException, IOException {
         // Get job name
         String jobName = build.getProject().getName();
+        Job<?, ?> job = build.getParent();
+        String jobPath = job.getFullName();
 
         // Get build number
         int buildNumber = build.getNumber();
@@ -102,23 +107,38 @@ public class InfluxDBPublisher extends Recorder {
         // Get build status
         String buildStatus = build.getResult().equals(Result.SUCCESS) ? "success" : "failed";
 
-        long durationMillis = build.getDuration();
+        long durationMillis = build.getEstimatedDuration();
+        long durationSeconds = durationMillis / 1000;
+        String durationString = build.getDurationString();
         // Get build time
+        Pattern pattern = Pattern.compile("\\d+\\.?\\d*");
+        Matcher matcher = pattern.matcher(durationString);
+        if (matcher.find()) {
+            numberStr = matcher.group(); // Safe to call after find()
+            System.out.println("Extracted number: " + numberStr);
+        }
+
         long buildTimeMillis = build.getStartTimeInMillis();
         String buildTime = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(buildTimeMillis);
 
         InfluxDBClient influxDBClient =
                 InfluxDBClientFactory.create(influxdbUri, adminToken.toCharArray(), organizationId, bucketName);
 
+        String[] jobTags = jobPath.split("/");
         listener.getLogger()
                 .println("Inserting jobName: " + jobName + ", buildNumber: " + buildNumber + ", buildStatus: "
-                        + buildStatus + ", buildTime: " + buildTime + ", durationMillis: " + durationMillis);
+                        + buildStatus + ", buildTime: " + buildTime + ", durationSeconds: " + durationSeconds
+                        + ", durationMillis: "
+                        + durationMillis + ", durationString: " + numberStr + ", jobPath: " + jobPath + ", jobTags: "
+                        + jobTags);
 
         WriteApiBlocking writeApi = influxDBClient.getWriteApiBlocking();
         Point point = Point.measurement("job_results")
-                .addTag("job_name", jobName)
+                .addTag("job_name", jobTags[2])
                 .addTag("build_status", buildStatus)
-                .addTag("build_duration", Double.toString(getRandomBuildDuration()))
+                .addTag("job_market", jobTags[0])
+                .addTag("job_environment", jobTags[1])
+                .addTag("build_duration", numberStr)
                 .addTag("build_number", Integer.toString(buildNumber))
                 .addField("build_time", buildTime)
                 .time(Instant.now().toEpochMilli(), WritePrecision.MS);
